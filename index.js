bfb43cadf1d958cec9b66d9365849a8365991db2
Vinicius Michelin da Silva <vinnimichelin@gmail.com>
2024-03-06 14:20:44 -0300

    Primeiro Commit - Todos os arquivos

---
+++ index.js
@@ -0,0 +1,24 @@
+function formatMoney(value){
+    value = Math.ceil(value *100) / 100
+    value = value.toFixed(2)
+    return 'R$ ' + value
+}
+
+function update(){
+    let bill = Number (document.getElementById('yourBill').value)
+    let tipPercent = document.getElementById('tipInput').value
+    let split = document.getElementById('splitInput').value
+
+    let tipValue = bill * (tipPercent/100)
+    let billTotal = bill + tipValue
+    let billEach = billTotal / split
+
+    
+    
+    document.getElementById('tipPercent').innerHTML = tipPercent + '%'
+    document.getElementById('splitValue').innerHTML = split + ' Pessoa(s)'
+    document.getElementById('tipValue').innerHTML = formatMoney(tipValue)
+    document.getElementById('totalWithTip').innerHTML = formatMoney(billTotal)
+    document.getElementById('billEach').innerHTML = formatMoney(billEach)
+
+}
